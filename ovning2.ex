ffb0aeb04d3654e7ae122bcbec0eb9558624bc61
ArianJavdan <javdan@kth.se>
2018-01-30 13:28:45 +0100

    Third commit

--- ovning2.ex
+++ ovning2.ex
@@ -7,7 +7,7 @@ defmodule Ovning2 do
 
 	def deriv({:const, _}, _) do ({:const, 0}) end
 	def deriv({:var, v}, {:var, v}) do ({:const, 1}) end
-	def deriv({:var, y}, _) do ({:var, y}) end
+	def deriv({:var, y}, _) do {:const, 0} end
 	def deriv({:mul, e1, e2}, v) do {:add, {:mul, deriv(e1, v), e2}, {:mul, e1, deriv(e2, v)}} end
 	def deriv({:add, e1, e2}, v) do {:add, deriv(e1, v), deriv(e2, v)} end
 
@@ -27,7 +27,7 @@ defmodule Ovning2 do
 	def oversimplify([a, b, c]) when (is_integer(a)) do
 		case{a, b} do
 			{0, '+'} -> "#{c}"
-			{0, '*'} -> "" 
+			{0, '*'} -> 0
 			{a,'*'} -> "#{a}#{c}"
 			{a,'+'} -> "(#{a}+#{c}) "
 		end
@@ -37,18 +37,25 @@ defmodule Ovning2 do
 		
 		case {c,b} do
 			{0, '+'} -> "#{a}"
-			{0, '*'} -> "" 
+			{0, '*'} -> 0 
 			{c,'*'} -> "#{c}#{a}"
 			{c,'+'} -> "(#{c}+#{a}) "
 		end
 	end
 
-	def oversimplify([a, b, c]) do oversimplify([oversimplify(a)] ++[b] ++[oversimplify(c)]) end
-	#def oversimplify([_, '*', 0]) do [] end
-	#def oversimplify([0, '*', _]) do [] end
-	##def oversimplify([a, '+', 0]) do a end
-	#def oversimplify([0, '+', a]) do a end
-	#def oversimplify(a) do a end'
+	def oversimplify([a, b, c]) do 
+		temp1 = oversimplify(a)
+		temp2 = oversimplify(c)
+		case [temp1, b, temp2] do
+			[0, '*', _] -> 0
+			[_, '*', 0] -> 0
+			[0, '+', temp2] -> temp2
+			[temp1, '+', 0] -> temp1
+			[temp1, b, temp2] -> oversimplify([temp1, b, temp2])
+		end
+		
+	end
+
 	
 	
 end
